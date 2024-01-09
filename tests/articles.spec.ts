c3087c5a4489913d5ef973bb9f1a75d824551a9f
Marcin <marcinkufel84@gmail.com>
2024-01-09 16:51:20 +0100

    chore: organize data in tests

--- tests/articles.spec.ts
+++ tests/articles.spec.ts
@@ -1,37 +1,71 @@
 import { expect, test } from "@playwright/test";
+import { randomNewArticle } from "../src/factories/article.factory";
+import { AddArticleModel } from "../src/models/article.model";
+import { ArticlePage } from "../src/pages/article.page";
 import { ArticlesPage } from "../src/pages/articles.page";
 import { LoginPage } from "../src/pages/login.page";
 import { testUser1 } from "../src/test-data/user.data";
 import { AddArticlesView } from "../src/views/add-article.view";
-import { ArticlePage } from "../src/pages/article.page";
-import { randomNewArticle } from "../src/factories/article.factory";
 
 test.describe("Verify articles", () => {
+  let loginPage: LoginPage;
+  let articlesPage: ArticlesPage;
+  let addArticleView: AddArticlesView;
+  let articleData: AddArticleModel;
+
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    articlesPage = new ArticlesPage(page);
+    addArticleView = new AddArticlesView(page);
+
+    await loginPage.goto();
+    await loginPage.login(testUser1);
+    await articlesPage.goto();
+
+    articleData = randomNewArticle();
+
+    await articlesPage.addArticleButtonLogged.click();
+    await expect.soft(addArticleView.header).toBeVisible();
+  });
   test("create new article @GAD-R04-01", async ({ page }) => {
     // Arrange
 
-    const loginPage = new LoginPage(page);
+    const articlePage = new ArticlePage(page);
+
     // Act
-    await loginPage.goto();
-    await loginPage.login(testUser1);
-
-    const articlesPage = new ArticlesPage(page);
-    await articlesPage.goto();
-
-    await articlesPage.addArticleButtonLogged.click();
-
-    const addArticleView = new AddArticlesView(page);
-    await expect.soft(addArticleView.header).toBeVisible();
-
-    const articleData = randomNewArticle();
 
     await addArticleView.createArticle(articleData);
-    // Assert
 
-    const articlePage = new ArticlePage(page);
+    // Assert
     await expect.soft(articlePage.articleTitle).toHaveText(articleData.title);
     await expect
       .soft(articlePage.articleBody)
       .toHaveText(articleData.body, { useInnerText: true });
+  });
+
+  test("reject creating article without title @GAD-R04-01", async () => {
+    // Arrange
+    const expectedErrorMessage = "Article was not created";
+
+    articleData.title = "";
+
+    // Act
+
+    await addArticleView.createArticle(articleData);
+    // Assert
+    await expect(addArticleView.alertPopup).toHaveText(expectedErrorMessage);
+  });
+
+  test("reject creating article without body @GAD-R04-01", async () => {
+    // Arrange
+    const expectedErrorMessage = "Article was not created";
+
+    articleData.body = "";
+
+    // Act
+
+    await addArticleView.createArticle(articleData);
+    // Assert
+    await expect(addArticleView.alertPopup).toHaveText(expectedErrorMessage);
   });
 });
