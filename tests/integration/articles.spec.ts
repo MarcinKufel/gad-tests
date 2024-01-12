c95c3a3edd3b8dc6b6c4fdc5bd7972c38071b07f
Marcin <marcinkufel84@gmail.com>
2024-01-12 15:47:57 +0100

    chore: refactor add article view to return PO

--- tests/integration/articles.spec.ts
+++ tests/integration/articles.spec.ts
@@ -93,15 +93,12 @@ test.describe("Verify articles", () => {
       await expect(addArticleView.alertPopup).toHaveText(expectedErrorMessage);
     });
 
-    test("create article with title with 128 signs @GAD-R04-02 @logged", async ({
-      page,
-    }) => {
+    test("create article with title with 128 signs @GAD-R04-02 @logged", async () => {
       // Arrange
-      const articlePage = new ArticlePage(page);
       const articleData = prepareRandomArticle(128);
 
       // Act
-      await addArticleView.createArticle(articleData);
+      const articlePage = await addArticleView.createArticle(articleData);
 
       // Assert
       await expect.soft(articlePage.articleTitle).toHaveText(articleData.title);
