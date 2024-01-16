7c96ed003b8add651ba76fe697ecb7f169cd7dc8
Marcin Kufel <marcinkufel84@gmail.com>
2024-01-16 20:14:21 +0100

    chore: own fixtures in setup tests

--- tests/setup/login.setup.ts
+++ tests/setup/login.setup.ts
@@ -1,15 +1,12 @@
-import { LoginPage } from "@_src/pages/login.page";
 import { testUser1 } from "@_src/test-data/user.data";
-import { expect, test as setup } from "@playwright/test";
+import { expect, test as setup } from "@_src/fixtures/merge.fixture";
 import { STORAGE_STATE } from "@_pw-config";
 
-setup("login and save session", async ({ page }) => {
+setup("login and save session", async ({ loginPage, page }) => {
   // Arrange
   const expectedWelcomeTitle = "Welcome";
-  const loginPage = new LoginPage(page);
 
   // Act
-  await loginPage.goto();
   const welcomePage = await loginPage.login(testUser1);
 
   const title = await welcomePage.getTitle();
